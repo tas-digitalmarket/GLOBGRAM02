580d4635d3f31bc006609b020eaa744c5339e7e9
tas-digitalmarket <tas.digimarket@gmail.com>
2025-07-16 13:37:26 -0700

    Update 26

--- globgram_p2p/macos/Flutter/GeneratedPluginRegistrant.swift
+++ globgram_p2p/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
-import firebase_auth
 import firebase_core
 import flutter_webrtc
 import path_provider_foundation
@@ -14,7 +13,6 @@ import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
