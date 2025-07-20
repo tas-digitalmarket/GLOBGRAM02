7b768975fdc37d4c9bbc30a373a7e39ac0729004
tas-digitalmarket <tas.digimarket@gmail.com>
2025-07-20 11:41:10 -0700

    Update44

--- globgram_p2p/test_import.dart
+++ globgram_p2p/test_import.dart
@@ -2,5 +2,5 @@ import 'package:globgram_p2p/features/chat/presentation/chat_page.dart';
 
 void main() {
   // Test import
-  const ChatPage(roomId: 'test');
+  const ChatPage(roomId: 'test', asCaller: false);
 }
